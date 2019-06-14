031525d63471fec9d8b79224c47db71f148496b6
Amaya76 <51099939+octubre25@users.noreply.github.com>
2019-06-14 09:22:29 -0600

    Add files via upload

---
+++ pizzeria_Jose.py
@@ -0,0 +1,44 @@
+Bienvenido a tostado´s pizza
+pizzeria Manuel = ["pizza con chamoiñones", "pizza con jamon",
+					"pizza con pepperoni", "pizza cuatro estaciones", "hot cheese"]
+
+def imprimir_menu():
+	print("1 Mostrar Pizza")
+	print("2 eliminar pizza")
+	print("3 Agregar pizza")
+	print("4 Salir")
+	valor = input("ingrese el valor de la accion: ")
+	return valor
+
+def mostrar_pizza ():
+	for pizza in pizzas
+
+def eliminar_pizza ():
+	pizza = []
+	
+def agregar_pizza (valor_pizza):
+	pizza.append(valor_pizza)
+
+
+ continuar = True
+
+while continuar:
+	#v_retornado = valor retornado
+	v_retornado = imprimir_menu()
+	if int(v_retornado) == 1:
+		for pizza in pizza
+			print(pizzas])
+		elif int(v_retornado) == 2:
+			pizzas = []
+		elif int(v_retornado) == 3:
+			valor = input("ingrese el pizza a agregar: ")
+			
+			pizzas.append(valor)
+		elif int(v_retornado) ==4:
+			continuar = False
+			
+		else:
+			print("opcion no controlada")
+			
+		
+
